52d63a1df9eb857347c5004b64d2562478b05e6b
sherif <sherif.elshamaaa@gmail.com>
2022-11-06 00:59:01 +0200

    u

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.eba-3pzm9stw.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://udagram-api.eba-3pzm9stw.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
