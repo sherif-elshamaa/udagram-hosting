2c2196c52682570a45f14abcec581fa991871d96
sherif <sherif.elshamaaa@gmail.com>
2022-11-05 22:21:01 +0200

    lint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-3pzm9stw.us-east-1.elasticbeanstalk.com/",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-3pzm9stw.us-east-1.elasticbeanstalk.com/',
 };
 
 /*
